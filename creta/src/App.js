b9437f4b8f57745fa12c8f519998830a72e0aab2
juan torrente <jnatorrente@gmail.com>
2022-11-23 18:28:39 +0100

    frame

--- creta/src/App.js
+++ creta/src/App.js
@@ -1,13 +1,38 @@
+import NavBar from './components/NavBar'
+import ViewPort from './components/ViewPort'
+import mainTheme from './style/mainTheme'
+import { CssBaseline, Box, Container, Toolbar } from '@mui/material'
+import { ThemeProvider } from '@emotion/react'
+
 function App() {
-  return (
-    <div>
-      <header>
-        <p>
-          hello, world
-        </p>
-      </header>
-    </div>
-  );
+
+	return (
+		<>
+			<ThemeProvider theme={mainTheme}>
+					<Box sx={{display: 'flex'}}>
+						<CssBaseline/>
+							<NavBar/>
+							<Box
+							component="main"
+							sx={{
+								backgroundColor: (theme) =>
+								theme.palette.mode === 'light'
+									? theme.palette.grey[100]
+									: theme.palette.grey[900],
+								flexGrow: 1,
+								height: '100vh',
+								overflow: 'auto',
+							}}
+							>
+								<Toolbar/>
+								<Container maxWidth="lg" sx={{mt:4, mb: 4}}> {/*el mx de este container es lo que crea el hueco en los lados de la ui*/}
+                    <ViewPort/>
+								</Container>
+							</Box>
+					</Box>
+			</ThemeProvider>
+		</>
+	);
 }
 
 export default App;
