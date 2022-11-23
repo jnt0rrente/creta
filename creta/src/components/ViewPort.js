53d4a463173460d3d5e3f41d5be6eec374979ccd
juan torrente <jnatorrente@gmail.com>
2022-11-23 19:02:04 +0100

    leer archivos

--- creta/src/components/ViewPort.js
+++ creta/src/components/ViewPort.js
@@ -1,9 +1,18 @@
+import CreateCustomButton from "./startpage/CreateCustomButton";
+import ImportButton from "./startpage/ImportButton";
+
+import { useState } from "react";
 import { Typography } from "@mui/material";
 
 export default function ViewPort() {
+    const [fileContent, setFileContent] = useState('')
 
     return (
-        <Typography>hello, world</Typography>
+        <>
+            <ImportButton setFileContent={setFileContent}/>
+            <CreateCustomButton/>
+            <Typography>{fileContent}</Typography>
+        </>
     );
 }
 
